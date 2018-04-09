e5acd158349ba22b5f26a49b61d4fed92ec6166b
Nadjakoroleva <nadin.koroleva@gmail.com>
2018-04-09 19:33:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    let narrowDate = document.querySelector('.menu-date');
+
+    narrowDate.addEventListener('click', function(evt) {
+        event.preventDefault();
+        console.log(1)
+    })
+}
